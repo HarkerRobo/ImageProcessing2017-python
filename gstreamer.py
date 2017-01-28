b520c381e05756b3a2fd2bc130a6deb4cdf99c49
Ryan Adolf <ryanadolf123@gmail.com>
2017-01-27 18:03:05 -0800

    Fix errors

--- gstreamer.py
+++ gstreamer.py
@@ -43,10 +43,10 @@ def webcam_streaming_pipeline(host, port):
 def webcam_loopback_command(caps):
     """
     Creates the command used by opencv to parse the raw video from the shared
-	memory location, given capture filters.
+    memory location, given capture filters.
 
-	These capture filters are needed as opencv needs to know how to parse
-	what is at the memory location (e.g. width and height).
+    These capture filters are needed as opencv needs to know how to parse what
+    is at the memory location (e.g. width and height).
     """
     return (
         'shmsrc socket-path={socket_path} ! '
@@ -55,23 +55,45 @@ def webcam_loopback_command(caps):
 
 def get_sink_caps(element):
     """
-    Returns the negotiated capture filters of a given element's sink
-	connection (i.e. what is outputted by the element).
+    Returns the negotiated capture filters of a given element's sink connection
+    (i.e. what is outputted by the element).
 
-	Because these capture filters will be negotiated, this method must be
-	used after the pipeline is playing.
+    Because these capture filters will be negotiated, this method must be used
+    after the pipeline is playing.
     """
     return element.get_static_pad('sink').get_current_caps()
 
+def get_cap_value_by_name(caps, name):
+    """
+    For some reason getValue(name) works for every data type except
+    fractions, so a bit more work needs to be done to get a capture filter by
+    its name.
+    """
+    try:
+        return caps.get_value(name)
+    except TypeError:
+        fraction = caps.get_fraction(name)
+        return '{1}/{2}'.format(*fraction)
+
 def make_command_line_parsable(caps):
+    """
+    A Cap object's toString method returns something very close to what this
+    method returns, but the outputted string also contains types in parenthesis
+    (e.g. width=(int)320). This method returns that string, but without the type
+    and parenthesis.
+
+    One could just use regex to find all ocurrences of \(.*?\), but that would
+    run into problems if the enclosed strings contained parenthesis.
+    """
+
     struct = caps.get_structure(0)
     out = struct.get_name()
 
     for i in range(struct.n_fields()):
-        capName = struct.nth_field_name(i)
-        capValue = struct.get_value(capName)
+        cap_name = struct.nth_field_name(i)
+        cap_value = get_cap_value_by_name(struct, cap_name)
 
-        out += ', {0}={1}'.format(capName, capValue)
+        out += ', {0}={1}'.format(cap_name, cap_value)
 
     return out
 
