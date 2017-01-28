2d0ccd795da2b000222208c760406cd2f844a377
Harker Software 1072 <harkersoftware1072@gmail.com>
2017-01-27 16:53:30 -0800

    added hello world example

--- gstreamer.py
+++ gstreamer.py
@@ -2,6 +2,7 @@ SOCKET_PATH = '/tmp/foo'
 STREAM_HOST = '169.254.84.224'
 STREAM_PORT = 5001
 
+import time
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
@@ -13,10 +14,7 @@ pipeline = Gst.parse_launch("videotestsrc ! glimagesink")
 # Start playing
 pipeline.set_state(Gst.State.PLAYING)
 
-# Wait until error or EOS
-bus = pipeline.get_bus()
-msg = bus.timed_pop_filtered(
-    Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
 
 # Free resources
-pipeline.set_state(Gst.State.NULL)
+#pipeline.set_state(Gst.State.NULL)
+time.sleep(4)
