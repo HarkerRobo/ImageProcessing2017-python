124c1724d7e8db8864b8724b7e5f6c4ed20f5bb1
Austin Zhang <azhang042@gmail.com>
2017-01-28 15:46:44 -0800

    Masks green-ish and performs a a closing-opening set to reduce noise

--- gearalign.py
+++ gearalign.py
@@ -1,13 +1,15 @@
 import cv2
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
 
-img = cv2.imread("pic.jpg", 0)
+img = cv2.imread("pic.jpg")
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-low_green = np.array([150,60,70])
-upper_green = np.array([175,80,90])
+
+low_green = np.array([60,100,80])
+upper_green = np.array([100,255,255])
 
 
 mask = cv2.inRange(src=hsv, lowerb=low_green, upperb=upper_green)
@@ -17,3 +19,17 @@ res = cv2.bitwise_and(src1=img, src2=img, mask=mask)
 cv2.imshow("original",img)
 cv2.imshow("mask", mask)
 cv2.imshow("res",res)
+
+se1 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(5,5))
+se2 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(2,2))
+mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_CLOSE, kernel=se1)
+mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_OPEN, kernel=se2)
+
+mask = np.dstack([mask, mask, mask]) / 255
+out = img * mask
+
+cv2.imshow("out", out)
+
+
+
+cv2.waitKey(0)
