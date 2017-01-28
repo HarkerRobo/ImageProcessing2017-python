866cacb547bad22092d8fd7ecb8d3a98010d6b7f
Ryan Adolf <ryanadolf123@gmail.com>
2017-01-27 16:47:37 -0800

    Paste tutorial code

--- gstreamer.py
+++ gstreamer.py
@@ -5,8 +5,18 @@ STREAM_PORT = 5001
 import gi
 gi.require_version('Gst', '1.0')
 from gi.repository import Gst
+Gst.init(None)
 
-Gst.init()
+# Build the pipeline
+pipeline = Gst.parse_launch("videotestsrc ! glimagesink")
 
-pipeline = Gst.Pipeline.new("autovideosrc ! glimagesink")
+# Start playing
 pipeline.set_state(Gst.State.PLAYING)
+
+# Wait until error or EOS
+bus = pipeline.get_bus()
+msg = bus.timed_pop_filtered(
+    Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.EOS)
+
+# Free resources
+pipeline.set_state(Gst.State.NULL)
