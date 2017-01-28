5aa0464c8092a5badf1dd55cb10c6f570d602402
Harker Software 1072 <harkersoftware1072@gmail.com>
2017-01-27 17:20:54 -0800

    Add example file again

---
+++ example.py
@@ -0,0 +1,17 @@
+SOCKET_PATH = '/tmp/foo'
+STREAM_HOST = '169.254.84.224'
+STREAM_PORT = 5001
+
+import time
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
+Gst.init(None)
+
+# Build the pipeline
+pipeline = Gst.parse_launch("videotestsrc ! glimagesink")
+
+# Start playing
+pipeline.set_state(Gst.State.PLAYING)
+
+time.sleep(4)
