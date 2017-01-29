b7faf13f379218cbe5b51b714484c5b631da1b26
Ryan Adolf <ryanadolf123@gmail.com>
2017-01-28 16:26:56 -0800

    Restructure gearalign code to be importable

--- gearalign.py
+++ gearalign.py
@@ -3,54 +3,55 @@ import numpy as np
 import time
 from matplotlib import pyplot as plt
 
+def process_image(img):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-img = cv2.imread("pic.jpg")
-
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-low_green = np.array([60,100,80])
-upper_green = np.array([100,255,255])
+    low_green = np.array([60,100,80])
+    upper_green = np.array([100,255,255])
 
 
-mask = cv2.inRange(src=hsv, lowerb=low_green, upperb=upper_green)
+    mask = cv2.inRange(src=hsv, lowerb=low_green, upperb=upper_green)
 
-res = cv2.bitwise_and(src1=img, src2=img, mask=mask)
+    res = cv2.bitwise_and(src1=img, src2=img, mask=mask)
 
-# cv2.imshow("original",img)
-# cv2.imshow("mask", mask)
-# cv2.imshow("res",res)
+    # cv2.imshow("original",img)
+    # cv2.imshow("mask", mask)
+    # cv2.imshow("res",res)
 
-# cv2.imshow("res", res)
+    # cv2.imshow("res", res)
 
 
 
-se1 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(5,5))
-se2 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(2,2))
-mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_CLOSE, kernel=se1)
-# cv2.imshow("mask1", mask)
-mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_OPEN, kernel=se2)
-# cv2.imshow("mask2", mask)
+    se1 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(5,5))
+    se2 = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=(2,2))
+    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_CLOSE, kernel=se1)
+    # cv2.imshow("mask1", mask)
+    mask = cv2.morphologyEx(src=mask, op=cv2.MORPH_OPEN, kernel=se2)
+    # cv2.imshow("mask2", mask)
 
 
-# mask = np.dstack([mask, mask, mask]) / 255
+    # mask = np.dstack([mask, mask, mask]) / 255
 
-out = cv2.bitwise_and(src1=res, src2=res, mask=mask)
+    out = cv2.bitwise_and(src1=res, src2=res, mask=mask)
 
-cv2.imshow("out", out)
+    cv2.imshow("out", out)
 
-# out = cv2.bilateralFilter(src=out, d=2, sigmaColor=5, sigmaSpace=5)
-out = cv2.blur(src=out, ksize=(5,5))
-cv2.imshow("blurred", out)
-# gray = cv2.cvtColor(src=out, code=cv2.COLOR)
-gray = cv2.cvtColor(src=out, code=cv2.COLOR_BGR2GRAY)
+    # out = cv2.bilateralFilter(src=out, d=2, sigmaColor=5, sigmaSpace=5)
+    out = cv2.blur(src=out, ksize=(5,5))
+    cv2.imshow("blurred", out)
+    # gray = cv2.cvtColor(src=out, code=cv2.COLOR)
+    gray = cv2.cvtColor(src=out, code=cv2.COLOR_BGR2GRAY)
 
-gray = np.float32(gray)
+    gray = np.float32(gray)
 
-dst = cv2.cornerHarris(src=gray, blockSize=2, ksize=3, k=0.04)
-dst = cv2.dilate(dst, None)
+    dst = cv2.cornerHarris(src=gray, blockSize=2, ksize=3, k=0.04)
+    dst = cv2.dilate(dst, None)
 
 
-out[dst>0.01*dst.max()] = [0,0,255]
-cv2.imshow("CURRENTFIN", out)
+    out[dst>0.01*dst.max()] = [0,0,255]
+    cv2.imshow("CURRENTFIN", out)
 
-cv2.waitKey(0)
+    cv2.waitKey(0)
+
+if __name__ == '__main__':
+    process_image(cv2.imread("pic.jpg"))
