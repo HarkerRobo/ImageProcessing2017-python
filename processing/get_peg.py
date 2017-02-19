408eccb05a04a14b259f8f0f6490381c2ad0c91a
Austin Zhang <azhang042@gmail.com>
2017-02-18 17:06:43 -0800

    commit

---
+++ processing/get_peg.py
@@ -0,0 +1,59 @@
+import freenect
+import cv2
+import numpy as np
+
+
+threshold = 100
+current_depth = 0
+
+
+def change_threshold(value):
+    global threshold
+    threshold = value
+
+
+def change_depth(value):
+    global current_depth
+    current_depth = value
+
+
+# def show_depth():
+#     global threshold
+#     global current_depth
+#
+#     depth, timestamp = freenect.sync_get_depth()
+#     depth = 255 * np.logical_and(depth >= current_depth - threshold,
+#                                  depth <= current_depth + threshold)
+#     depth = depth.astype(np.uint8)
+#     cv2.imshow('Depth', depth)
+
+
+def mark_closest():
+    depth, timestamp = freenect.sync_get_depth()
+
+    loc = np.unravel_index(depth.argmax(), depth.shape)
+    loc = tuple(loc)
+    depth[loc[0]-5:loc[0]+5 , loc[1]-5:loc[1]+5]  = [0,255,0]
+    cv2.imshow('Depth', rgb_to_bgr(depth))
+
+
+
+def show_video():
+    cv2.imshow('Video', rgb_to_bgr(freenect.sync_get_video()[0]))
+
+
+# cv2.namedWindow('Depth')
+# cv2.namedWindow('Video')
+# cv2.createTrackbar('threshold', 'Depth', threshold,     500,  change_threshold)
+# cv2.createTrackbar('depth',     'Depth', current_depth, 2048, change_depth)
+
+print('Press ESC in window to stop')
+
+def rgb_to_bgr(video):
+    return video[:, :, ::-1]
+    cv2.circle()
+while 1:
+    mark_closest()
+    show_video()
+    if cv2.waitKey(10) == 27:
+        break
--- processing/stereo.py
+++ processing/stereo.py
@@ -21,8 +21,9 @@ def process(left_img, right_img):
     right_dict = calibrate_camera.calibrate(
         r'C:\Users\Austin\Desktop\roboimage\ImageProcessing2017-python\processing\chess_right*')
 
-    corners_left = None  # corner finding
-    corner_right = None  # corner finding
+    corners_left = np.concatenate(tapecontours.get_corners_from_image(left_img))
+    corners_right = np.concatenate(tapecontours.get_corners_from_image(right_img))
+
 
     # reproj_error, camera_matrix,  distance_coeffs, rvecs, tvecs, obj_points, img_points, img_size
 
@@ -53,12 +54,12 @@ def process(left_img, right_img):
     # left_img_remap = cv2.remap(left_img, left_maps[0], left_maps[1], cv2.INTER_LANCZOS4)
     # right_img_remap = cv2.remap(right_img, right_maps[0], right_maps[1], cv2.INTER_LANCZOS4)
 
-    corners_left = [[(402, 434), (402, 327), (448, 327), (448, 434)], [(242, 421), (242, 324), (290, 324), (290, 421)]]
-
-
-    corners_right = [[(508, 430), (508, 324), (552, 324), (552, 430)], [(342, 427), (342, 326), (384, 326), (384, 427)]]
-    corners_left= np.asarray(corners_left, dtype=np.float32)
-    corners_right= np.asarray(corners_right, dtype=np.float32)
+    # corners_left = [[(402, 434), (402, 327), (448, 327), (448, 434)], [(242, 421), (242, 324), (290, 324), (290, 421)]]
+    #
+    #
+    # corners_right = [[(508, 430), (508, 324), (552, 324), (552, 430)], [(342, 427), (342, 326), (384, 326), (384, 427)]]
+    # corners_left= np.asarray(corners_left, dtype=np.float32)
+    # corners_right= np.asarray(corners_right, dtype=np.float32)
 
 
     reconstructed_points = []
@@ -67,9 +68,9 @@ def process(left_img, right_img):
         disparity = point_pair["left"]["x"] - point_pair["right"]["x"]
         reconstructed_point = (point_pair["left"]["x"], point_pair["left"]["y"], disparity)
         reconstructed_points.append(reconstructed_point)
-    points_left = None
-    points_right = None
-    mystery = cv2.triangulatePoints(projMatr1=P1, projMatr2=P2, projPoints1=points_left, projPoints2=points_right)
+
+    mystery = cv2.triangulatePoints(projMatr1=P1, projMatr2=P2, projPoints1=corners_left, projPoints2=corners_right)
+    print(mystery)
     # Z = (b * f) / (x1 - x2)
     # b = distance between lens
     # distance = (camera_dist_between_lenses*focal length)/(disparity)
