25dab51c77d4bc1ae7bf23823a07da39ecea7110
Ryan Adolf <ryanadolf123@gmail.com>
2017-03-11 11:14:57 -0800

    Don't skip itegration test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -76,7 +76,6 @@ class IntegrationTest(unittest.TestCase):
         self.cap2 = None
 
     # TODO: Make this test work
-    @unittest.skip('known to never work even when source code works')
     def test_works(self):
         """Test that the stream can be correctly read."""
 
